a7d28c18a1535026e9f0ec43fec7fcb3473489a2
cutlerwater <cutlerwater2@live.com>
2021-09-29 10:21:14 -0400

    second upload

--- Project/settings.py
+++ Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jtkmz^b26^w*&$sc7d7*29%q($18wmt8bbv9u$-dtiyceyso0@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
